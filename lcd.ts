018844dac43474dc063d5b04334e00e348453a08
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2019-05-09 09:50:04 +0900

    Update lcd.ts

--- lcd.ts
+++ lcd.ts
@@ -20,7 +20,7 @@ namespace lcd {
         let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
         return val;
     }
-/*
+
     function initAQM(): void {
         i2cwrite(AQM_ADDRESS, 0x00, 0x39); control.waitMicros(50);
         i2cwrite(AQM_ADDRESS, 0x00, 0x11); control.waitMicros(50);
@@ -47,18 +47,18 @@ namespace lcd {
 	}
 
     }
-*/
+
     //  subcategory="LCD"
     //% blockId="show_string"
     //% block="show string %text"
     //% weight=80
     export function showString(text: string): void {
-/*
+
       lcdOut(0, text);
       if ( text.lengrh() > LCD_SIZE_X ) {
           let str = text.substring(LCD_SIZE_X, text.lengrh());
-          lcdOut(1, text);
+//          lcdOut(1, text);
       }
-*/
+
     }
 } 
