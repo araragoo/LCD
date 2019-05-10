67e299b0cfd35f30f911e8d47d5a2b425e3cf881
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2019-05-10 22:48:43 +0900

    Update lcd.ts

--- lcd.ts
+++ lcd.ts
@@ -66,9 +66,11 @@ namespace lcd {
               initAQM();
         }
         lcdOut(0, text);
+/*
         if ( text.lengrh() > LCD_SIZE_X ) {
             let str = text.substring(LCD_SIZE_X, text.lengrh());
             lcdOut(1, str);
         }
+*/
     }
 } 
