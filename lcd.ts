bc226451ab5f730890ed4be90addbf92b74e1997
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2019-05-10 21:58:46 +0900

    add new file

--- lcd.ts
+++ lcd.ts
@@ -52,7 +52,7 @@ namespace lcd {
         control.waitMicros(1000);
         for (let i = 0; i < LCD_SIZE_X; i++ ) {
 //            if (text.charCodeAt(i) == 0x00) return;
-            i2cwrite(AQM_ADDRESS, 0x40, text.charCodeAt(i));
+            i2cwrite(AQM_ADDRESS, 0x40, text.charAt(i));
             control.waitMicros(100);
         }
     }
