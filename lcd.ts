405190a019b06fc90a5dec3ec9e927567a203ba7
araragoo <arara_goo@yahoo.co.jp>
2019-05-12 15:35:55 +0900

    Update lcd.ts

--- lcd.ts
+++ lcd.ts
@@ -51,9 +51,7 @@ namespace lcd {
         control.waitMicros(1000);
         for (let i = 0; i < LCD_SIZE_X; i++ ) {
             //if (text.charAt(i) == 0x00) return;
-            if (text.substr(i, 1) == 0x00) return;
             //i2cwrite(AQM_ADDRESS, 0x40, text.charAt(i));
-            i2cwrite(AQM_ADDRESS, 0x40, text.substr(i, 1));
             control.waitMicros(100);
         }
 
